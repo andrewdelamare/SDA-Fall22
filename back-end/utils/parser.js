7301ec9858ff1e9b1948a3962946beeed10ecaa5
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-07-05 21:29:03 +0300

    Improve parse and cleanup keys, filter invalid results durring parse

--- back-end/utils/parser.js
+++ back-end/utils/parser.js
@@ -3,24 +3,28 @@ const arg = process.argv[2];
 const { parse } = require("csv-parse");
 
 const results = [];
-
-fs.createReadStream(`csv/${arg}`)
+const filtered = [];
+fs.createReadStream(`${arg}`)
   .pipe(parse({ columns: true }))
-  .on("data", (data) => results.push(data))
+  .on("data", (data) => {
+    const vals = Object.values(data);
+    const fixed = {
+      departure: vals[0],
+      return: vals[1],
+      depId: vals[2],
+      depNm: vals[3],
+      retId: vals[4],
+      retNm: vals[5],
+      distance: vals[6],
+      duration: vals[7],
+    };
+    parseInt(fixed.duration) <= 10 ?
+    filtered.push(fixed) :
+    parseInt(fixed.distance) <= 10 ?
+    filtered.push(fixed) :
+    results.push(fixed);
+  })
   .on("end", () => {
-    const fixedKeys = results.map((obj) => {
-      const vals = Object.values(obj);
-      const fixed = {
-        departure: vals[0],
-        return: vals[1],
-        depId: vals[2],
-        depNm: vals[3],
-        retId: vals[4],
-        retNm: vals[5],
-        distance: vals[6],
-        duration: vals[7],
-      };
-      return fixed;
-    });
-    console.log(fixedKeys);
+    console.log("results: ",results.length);
+    console.log("filtered out: ",filtered.length);
   });
