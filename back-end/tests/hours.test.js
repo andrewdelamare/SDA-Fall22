ddc5d34d490a6393e54832ed2c88c51bb98ebe24
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-07-18 16:48:49 +0300

    Split up parser into seperate functions for testing. Add npm test script

--- back-end/tests/hours.test.js
+++ back-end/tests/hours.test.js
@@ -2,13 +2,24 @@ const { default: mongoose } = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const hourRouter = require("../controllers/hours");
-const Trip = require("../models/trip");
-const processFile = require("../utils/parser");
-const testFile = require("test");
+const Hour = require("../models/hour");
+const {
+  processFile,
+  parseFile,
+  packageHours,
+  uploadFiles,
+} = require("../utils/parser");
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Trip.deleteMany();
-});
+//beforeEach(async () => {
+//await Hour.deleteMany();
+//});
 
-test("Parse files", () => {});
+test("Parse files", async () => {
+  //expect.assertions(1);
+  const obj = await parseFile("./test.csv", "trip");
+  console.log(obj);
+
+  //console.log(result.recordsLen);
+  //const hours = result.hours;
+}, 30000);
--- back-end/utils/parser.js
+++ back-end/utils/parser.js
@@ -18,130 +18,155 @@ const {
 //Process file function parses csv files, creates new Trip/Station objects, filters them,
 //adds them to containers by date and hour to aid query time and uploads them to a database
 //node utils/parser.js csv/2021-05.csv
-const processFile = async (file, type, upload) => {
-  const start = Date.now();
+
+const parseFile = (file, type) => {
   let records = [];
-  let hours = [];
+  let recordsLen = 0;
   let trash = 0;
   const parser = fs.createReadStream(`${file}`);
   console.log("now parsing file...");
-  parser
-    .pipe(
-      parse({
-        columns: true,
-      })
-    )
-    .on("data", async (record) => {
-      let vals = Object.values(record);
-      if (type === "trip") {
-        let it = {
-          departure: vals[0],
-          ret: vals[1],
-          depId: vals[2],
-          depNm: vals[3],
-          retId: vals[4],
-          retNm: vals[5],
-          distance: vals[6],
-          duration: vals[7],
-        };
-        parseInt(it.distance) > 10 && parseInt(it.duration) > 10
-          ? records.push(it)
-          : trash++;
-      }
-    })
-    .on("end", async () => {
-      console.log("all records parsed");
-      console.log(records.length, " valid records");
-      console.log(trash, " invalid records");
-      const dep = parseISO(records[0].departure);
-      const startmon = startOfMonth(dep);
-      const endmon = endOfMonth(dep);
-      const hourinterval = eachHourOfInterval({ start: startmon, end: endmon });
-      for (const h of hourinterval) {
-        hours.push(
-          new Hour({
-            day: startOfDay(h),
-            hour: h,
-            trips: [],
-          })
-        );
-      }
-      const progbar = new cliProgress.SingleBar(
-        {},
-        cliProgress.Presets.shades_classic
-      );
-      const leng = records.length;
-      console.log("now sorting trips into their respective hour documents...");
-      progbar.start(leng, 0);
-      for (const record of records) {
-        const dep = parseISO(record.departure);
-        const starthour = startOfHour(dep);
-        progbar.increment();
-        for (const h of hours) {
-          if (!(h.hour > starthour) && !(h.hour < starthour)) {
-            h.trips.push(record);
-          }
-        }
-      }
-      records = [];
-      progbar.stop();
-      if (upload === "upload") {
-        try {
-          mongoose.connect(config.MONGODB_URI);
-          const chunkedUpload = async (arr, model) => {
-            console.log("now sending chunks of 100 records to db...");
-            const progbar = new cliProgress.SingleBar(
-              {},
-              cliProgress.Presets.shades_classic
-            );
-            const leng = ~~(arr.length / 100) + 1;
-            progbar.start(leng, 0);
-            let chunks = 0;
-            for (let i = 0; i < arr.length; i += 100) {
-              const chunk = arr.slice(i, i + 100);
-              const done = await model.insertMany(chunk, { ordered: false });
-              if (done) {
-                chunks++;
-                progbar.increment();
-              }
-            }
-            progbar.stop();
+  return new Promise((resolve) => {
+    parser
+      .pipe(
+        parse({
+          columns: true,
+        })
+      )
+      .on("data", (record) => {
+        let vals = Object.values(record);
+        if (type === "trip") {
+          let it = {
+            departure: vals[0],
+            ret: vals[1],
+            depId: vals[2],
+            depNm: vals[3],
+            retId: vals[4],
+            retNm: vals[5],
+            distance: vals[6],
+            duration: vals[7],
           };
-          await chunkedUpload(hours, Hour);
-          const mls = Date.now() - start;
-          console.log(
-            "It took ",
-            Math.floor(mls / 1000),
-            " seconds to complete parsing and upload of your file"
-          );
-          mongoose.connection.close(function () {
-            console.log("complete");
-            process.exit(0);
-          });
-        } catch (error) {
-          console.log(error);
-          mongoose.connection.close(function () {
-            console.log("closing mongo connection");
-            process.exit(0);
-          });
+          parseInt(it.distance) > 10 && parseInt(it.duration) > 10
+            ? records.push(it)
+            : trash++;
         }
-      } else {
-        const mls = Date.now() - start;
-        console.log(
-          "It took ",
-          Math.floor(mls / 1000),
-          " seconds to complete the parsing of your file"
-        );
-        process.exit(0);
-      }
-    });
+      })
+      .on("end", () => {
+        console.log("all records parsed");
+        console.log(records.length, " valid records");
+        console.log(trash, " invalid records");
+        recordsLen = records.length;
+        resolve({ records, recordsLen, trash });
+      });
+  });
 };
+
+const packageHours = (records) => {
+  const hours = [];
+  const dep = parseISO(records[0].departure);
+  const startmon = startOfMonth(dep);
+  const endmon = endOfMonth(dep);
+  const hourinterval = eachHourOfInterval({ start: startmon, end: endmon });
+  return new Promise((resolve) => {
+    for (const h of hourinterval) {
+      hours.push(
+        new Hour({
+          day: startOfDay(h),
+          hour: h,
+          trips: [],
+        })
+      );
+    }
+    const progbar = new cliProgress.SingleBar(
+      {},
+      cliProgress.Presets.shades_classic
+    );
+    const leng = records.length;
+    console.log("now sorting trips into their respective hour documents...");
+    progbar.start(leng, 0);
+    for (const record of records) {
+      const dep = parseISO(record.departure);
+      const starthour = startOfHour(dep);
+      progbar.increment();
+      for (const h of hours) {
+        if (!(h.hour > starthour) && !(h.hour < starthour)) {
+          h.trips.push(record);
+        }
+      }
+    }
+
+    progbar.stop();
+    resolve(hours);
+  });
+};
+
+const chunkedUpload = async (arr, model) => {
+  console.log("now sending chunks of 100 records to db...");
+  return new Promise(async (resolve) => {
+    const progbar = new cliProgress.SingleBar(
+      {},
+      cliProgress.Presets.shades_classic
+    );
+    const leng = ~~(arr.length / 100) + 1;
+    progbar.start(leng, 0);
+    for (let i = 0; i < arr.length; i += 100) {
+      const chunk = arr.slice(i, i + 100);
+      const done = await model.insertMany(chunk, { ordered: false });
+      if (done) {
+        progbar.increment();
+      }
+    }
+    progbar.stop();
+    resolve();
+  });
+};
+
+const uploadFiles = async (arr, model) => {
+  try {
+    mongoose.connect(config.MONGODB_URI);
+    await chunkedUpload(arr, model);
+    mongoose.connection.close(function () {
+      console.log("complete");
+    });
+  } catch (error) {
+    console.log(error);
+    mongoose.connection.close(function () {
+      console.log("closing mongo connection");
+      process.exit(0);
+    });
+  }
+};
+
+const processFile = async (file, type, action) => {
+  const start = Date.now();
+  const recordsObj = await parseFile(file, type);
+  const records = recordsObj.records;
+  const hours = await packageHours(records);
+  if (action === "upload" && type == "trip") {
+    await uploadFiles(hours, Hour);
+    const mls = Date.now() - start;
+    console.log(
+      "It took ",
+      Math.floor(mls / 1000),
+      " seconds to complete parsing and upload of your file"
+    );
+    process.exit(0);
+  } else {
+    const mls = Date.now() - start;
+    console.log(
+      "It took ",
+      Math.floor(mls / 1000),
+      " seconds to complete the processing of your file"
+    );
+    process.exit(0);
+  }
+};
+
 const file = process.argv[2];
 const type = process.argv[3];
-const upload = process.argv[4];
+const action = process.argv[4];
 try {
-  processFile(file, type, upload);
+  processFile(file, type, action);
 } catch (error) {
   console.log(error);
 }
-module.exports = processFile;
+module.exports = { processFile, parseFile, packageHours, uploadFiles };
