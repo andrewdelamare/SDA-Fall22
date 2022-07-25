0fb2eddb089ade0831d08b7a088f0196472ec306
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-07-25 12:33:59 +0300

    Modify calendar component for this app, limit date selection, display one month at a time, etc

--- front-end/src/components/Calendar.js
+++ front-end/src/components/Calendar.js
@@ -7,32 +7,18 @@ import {
   parseISO,
   endOfDay,
   startOfDay,
+  toDate,
 } from "date-fns";
 import { startOfToday } from "date-fns";
 import { useEffect, useState } from "react";
 
 // ------------------------------------------------------------------------
 
-const Date = ({
-  selectDay,
-  day,
-  selectedDayStart,
-  selectedDayEnd,
-  reserved,
-}) => {
-  const [ciRes, setCiRes] = useState("");
-  const [coRes, setCoRes] = useState("");
-
-  const today = startOfToday();
+const Day = ({ selectDay, day, selectedDay }) => {
+  const today = new Date(2021, 3, 30);
+  const end = new Date(2021, 7, 1);
   const dayOfWeek = day.getDay();
-  let selStart =
-    endOfDay(day).getTime() === selectedDayStart.getTime() ? true : false;
-  let selEnd = false;
-  if (selectedDayEnd !== null) {
-    if (startOfDay(day).getTime() === selectedDayEnd.getTime()) {
-      selEnd = true;
-    }
-  }
+  let sel = startOfDay(day).getTime() === selectedDay.getTime() ? true : false;
 
   let stylez = "";
   switch (dayOfWeek) {
@@ -70,45 +56,21 @@ const Date = ({
   }
 
   const selectDayClick = () => {
-    selectDay(day);
+    day < today
+      ? console.log("no data for this day, please select another")
+      : day > end
+      ? console.log("no data for this day, please select another")
+      : selectDay(day);
   };
   const selected = "bg-black text-white rounded-full";
-  let selStylez =
-    selStart !== false ? selected : selEnd !== false ? selected : "";
+  let selStylez = sel !== false ? selected : "";
 
   const invalidStylez = "line-through text-slate-500 decoration-red-500";
   let beforeToday = day < today ? invalidStylez : "";
-  let beforeSelStart =
-    endOfDay(day) < selectedDayStart && selectedDayEnd === null
-      ? invalidStylez
-      : "";
-
-  const end = endOfDay(day);
-  const start = startOfDay(day);
-  const reset = () => {
-    setCiRes("");
-    setCoRes("");
-  };
-
-  useEffect(() => {
-    if (reserved[0] !== null) {
-      let cIreserved = reserved.find((d) => d.getTime() === end.getTime());
-      let cOreserved = reserved.find((d) => d.getTime() === start.getTime());
-      selectedDayStart !== null &&
-      selectedDayEnd !== null &&
-      selectedDayEnd.getTime() === start.getTime()
-        ? setCiRes("")
-        : cOreserved !== undefined && selectedDayEnd === null
-        ? setCoRes(invalidStylez)
-        : cIreserved !== undefined && selectedDayEnd != null
-        ? setCiRes(invalidStylez)
-        : reset();
-    }
-  });
-
+  let afterEnd = day > end ? invalidStylez : "";
   return (
     <div
-      className={`${stylez} ${selStylez} ${beforeToday} ${beforeSelStart} ${ciRes} ${coRes} `}
+      className={`${stylez} ${selStylez} ${beforeToday} ${afterEnd}`}
       role="button"
       onClick={selectDayClick}
     >
@@ -119,14 +81,7 @@ const Date = ({
 
 // ------------------------------------------------------------------------
 
-const Month = ({
-  month,
-  className,
-  selectedDayStart,
-  selectedDayEnd,
-  selectDay,
-  reserved,
-}) => {
+const Month = ({ month, className, selectedDay, selectDay }) => {
   const intYr = month.yr;
   const intMon = month.mon < 10 ? `0${month.mon}` : month.mon;
   const day = parseISO(`${intYr}-${intMon}-15`);
@@ -157,13 +112,11 @@ const Month = ({
       </div>
       <div className="grid gap-3 grid-cols-7 pt-3 mx-4">
         {allDays.map((day) => (
-          <Date
+          <Day
             day={day}
             key={day}
             selectDay={selectDay}
-            selectedDayStart={selectedDayStart}
-            selectedDayEnd={selectedDayEnd}
-            reserved={reserved}
+            selectedDay={selectedDay}
           />
         ))}
       </div>
@@ -173,29 +126,18 @@ const Month = ({
 
 // ------------------------------------------------------------------------
 
-export const Calendar = ({
-  setSelectedDayStart,
-  setSelectedDayEnd,
-  selectedDayStart,
-  selectedDayEnd,
-  reserved,
-  filldates,
-}) => {
-  const today = startOfToday();
+export const Calendar = ({ setSelectedDay, selectedDay, filldates }) => {
+  const today = toDate(new Date(2021, 3, 1));
   let month = getMonth(today) + 1;
   let year = getYear(today);
   const [selectedMonth, setSelectedMonth] = useState({ mon: month, yr: year });
   const [smStyleState, setSmS] = useState("w-96");
   const [nextMonth, setNextMonth] = useState({
-    mon: month === 12 ? 1 : month + 1,
-    yr: month === 12 ? year + 1 : year,
+    mon: month === 7 ? 7 : month + 1,
+    yr: 2021,
   });
-  const [nmStyleState, setNmS] = useState("w-96");
-  const [nextNextMonth, setNextNextMonth] = useState({
-    mon: month === 12 ? 2 : month === 11 ? 1 : month + 2,
-    yr: month === 12 ? year + 1 : month === 11 ? year + 1 : year,
-  });
-  const [nnmStyleState, setNnMs] = useState("opacity-0 w-96 invisible");
+  const [nmStyleState, setNmS] = useState("opacity-0 w-96 invisible");
+
   const [lastMonth, setLastMonth] = useState({
     mon: month === 1 ? 12 : month - 1,
     yr: month === 1 ? year - 1 : year,
@@ -211,292 +153,60 @@ export const Calendar = ({
     "opacity-0 w-96 transition duration-300 -translate-x-full  ";
   const smTransitionR = " transition w-96 duration-300 translate-x-full";
 
-  const nmStyles = "opacity-100 w-96";
-  const nmTransitionL = " transition w-96 duration-300 -translate-x-full ";
-  const nmTransitionR =
-    "transition w-96 duration-300 opacity-0 translate-x-full";
+  //const nmStyles = "opacity-100 w-96";
+  //const nmTransitionL = " transition w-96 duration-300 -translate-x-full ";
+  //const nmTransitionR = "transition w-96 duration-300 opacity-0 translate-x-full";
 
-  const nNmStyles = "opacity-0 w-96 invisible";
-  const nNmTransitionL =
+  const nmStyles = "opacity-0 w-96 invisible";
+  const nmTransitionL =
     "opacity-100 w-96 transition duration-300 -translate-x-full";
 
-  const determineRes = (day, inOut) => {
-    const end = endOfDay(day);
-    const start = startOfDay(day);
-    let cIreserved = reserved.find((d) => d.getTime() === end.getTime());
-    let cOreserved = reserved.find((d) => d.getTime() === start.getTime());
-
-    return inOut === "in" && cIreserved === undefined
-      ? false
-      : inOut === "in" && cIreserved !== undefined
-      ? true
-      : inOut === "out" && cOreserved === undefined
-      ? false
-      : inOut === "out" && cOreserved !== undefined
-      ? true
-      : false;
-  };
-
-  const detInclRes = (end) => {
-    const gtRes = reserved.map((date) => date.getTime());
-    const filled = filldates(selectedDayStart, end);
-    const fillmap = filled.map((date) => date.getTime());
-    const resArr = gtRes.map((date) => fillmap.includes(date));
-    return resArr.includes(true) ? true : false;
-  };
-
   const selectDay = (date) => {
-    switch (true) {
-      case date < today:
-        console.log("Please select a day starting from today");
-        break;
-      case date > selectedDayStart && selectedDayEnd !== null:
-        if (determineRes(date, "in") === false) {
-          setSelectedDayStart(endOfDay(date));
-          setSelectedDayEnd(null);
-        } else {
-          window.alert(
-            "Check in for this day is already reserved. Please choose another date for your stay."
-          );
-        }
-        break;
-      case date < selectedDayStart && selectedDayEnd !== null:
-        if (determineRes(date, "in") === false) {
-          setSelectedDayStart(endOfDay(date));
-          setSelectedDayEnd(null);
-        } else {
-          window.alert(
-            "Check in for this day is already reserved. Please choose another date for your stay."
-          );
-        }
-        break;
-      case date > selectedDayStart && selectedDayEnd === null:
-        if (determineRes(date, "out") === false && detInclRes(date) === false) {
-          setSelectedDayEnd(startOfDay(date));
-        } else {
-          window.alert(
-            "The dates you selected are not available. Please choose another check out date."
-          );
-        }
-
-        break;
-      default:
-        console.log("Please choose a checkout day");
-    }
+    setSelectedDay(date);
   };
 
   const updateMonth = (direction) => {
     switch (direction) {
       case "+":
-        if (selectedMonth.mon === 11) {
-          //move calendars
-          setSmS(smTransitionL);
-          setNmS(nmTransitionL);
-          setNnMs(nNmTransitionL);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setSelectedMonth({
-              mon: selectedMonth.mon + 1,
-              yr: selectedMonth.yr,
-            });
-            setNextMonth({ mon: 1, yr: nextMonth.yr + 1 });
-            setNextNextMonth({
-              mon: nextNextMonth.mon + 1,
-              yr: nextNextMonth.yr,
-            });
-            //revert styles
-            setSmS(smStyles);
-            setNmS(nmStyles);
-            setNnMs(nNmStyles);
-          }, 300);
+        //move calendars
+        setSmS(smTransitionL);
+        setNmS(nmTransitionL);
 
-          break;
-        } else if (selectedMonth.mon === 12) {
-          //move calendars
-          setSmS(smTransitionL);
-          setNmS(nmTransitionL);
-          setNnMs(nNmTransitionL);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setSelectedMonth({ mon: 1, yr: selectedMonth.yr + 1 });
-            setNextMonth({ mon: 2, yr: nextMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon + 1,
-              yr: nextNextMonth.yr,
-            });
-            //revert styles
-            setSmS(smStyles);
-            setNmS(nmStyles);
-            setNnMs(nNmStyles);
-          }, 300);
+        setTimeout(() => {
+          //set new data
+          setLastMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
+          setSelectedMonth({
+            mon: selectedMonth.mon + 1,
+            yr: selectedMonth.yr,
+          });
+          setNextMonth({ mon: nextMonth.mon + 1, yr: nextMonth.yr });
 
-          break;
-        } else if (selectedMonth.mon === 10) {
-          //move calendars
-          setSmS(smTransitionL);
-          setNmS(nmTransitionL);
-          setNnMs(nNmTransitionL);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setSelectedMonth({
-              mon: selectedMonth.mon + 1,
-              yr: selectedMonth.yr,
-            });
-            setNextMonth({ mon: nextMonth.mon + 1, yr: nextMonth.yr });
-            setNextNextMonth({ mon: 1, yr: nextNextMonth.yr + 1 });
-            //revert styles
-            setSmS(smStyles);
-            setNmS(nmStyles);
-            setNnMs(nNmStyles);
-          }, 300);
+          //revert styles
+          setSmS(smStyles);
+          setNmS(nmStyles);
+        }, 300);
 
-          break;
-        } else {
-          //move calendars
-          setSmS(smTransitionL);
-          setNmS(nmTransitionL);
-          setNnMs(nNmTransitionL);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setSelectedMonth({
-              mon: selectedMonth.mon + 1,
-              yr: selectedMonth.yr,
-            });
-            setNextMonth({ mon: nextMonth.mon + 1, yr: nextMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon + 1,
-              yr: nextNextMonth.yr,
-            });
-            //revert styles
-            setSmS(smStyles);
-            setNmS(nmStyles);
-            setNnMs(nNmStyles);
-          }, 300);
-
-          break;
-        }
+        break;
       case "-":
-        if (selectedMonth.mon === 1) {
-          //move calendars
-          setLmS(lmTransitionR);
-          setSmS(smTransitionR);
-          setNmS(nmTransitionR);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: lastMonth.mon - 1, yr: selectedMonth.yr });
-            setNextMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon - 1,
-              yr: nextNextMonth.yr,
-            });
-            setSelectedMonth({ mon: 12, yr: selectedMonth.yr - 1 });
-            //revert styles
-            setLmS(lmStyles);
-            setSmS(smStyles);
-            setNmS(nmStyles);
-          }, 300);
+        //move calendars
+        setLmS(lmTransitionR);
+        setSmS(smTransitionR);
+        setTimeout(() => {
+          //set new data
+          setLastMonth({ mon: lastMonth.mon - 1, yr: selectedMonth.yr });
+          setNextMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
 
-          break;
-        } else if (selectedMonth.mon === 2) {
-          //move calendars
-          setLmS(lmTransitionR);
-          setSmS(smTransitionR);
-          setNmS(nmTransitionR);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: 12, yr: lastMonth.yr - 1 });
-            setNextMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon - 1,
-              yr: nextNextMonth.yr,
-            });
-            setSelectedMonth({
-              mon: selectedMonth.mon - 1,
-              yr: selectedMonth.yr,
-            });
-            //revert styles
-            setLmS(lmStyles);
-            setSmS(smStyles);
-            setNmS(nmStyles);
-          }, 300);
+          setSelectedMonth({
+            mon: selectedMonth.mon - 1,
+            yr: selectedMonth.yr,
+          });
+          //revert styles
+          setLmS(lmStyles);
+          setSmS(smStyles);
+          setNmS(nmStyles);
+        }, 300);
 
-          break;
-        } else if (selectedMonth.mon === 12) {
-          //move calendars
-          setLmS(lmTransitionR);
-          setSmS(smTransitionR);
-          setNmS(nmTransitionR);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: lastMonth.mon - 1, yr: selectedMonth.yr });
-            setSelectedMonth({
-              mon: selectedMonth.mon - 1,
-              yr: selectedMonth.yr,
-            });
-            setNextMonth({ mon: 12, yr: nextMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon - 1,
-              yr: nextNextMonth.yr,
-            });
-
-            //revert styles
-            setLmS(lmStyles);
-            setSmS(smStyles);
-            setNmS(nmStyles);
-          }, 300);
-
-          break;
-        } else if (selectedMonth.mon === 11) {
-          //move calendars
-          setLmS(lmTransitionR);
-          setSmS(smTransitionR);
-          setNmS(nmTransitionR);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: lastMonth.mon - 1, yr: selectedMonth.yr });
-            setSelectedMonth({
-              mon: selectedMonth.mon - 1,
-              yr: selectedMonth.yr,
-            });
-            setNextMonth({ mon: nextMonth.mon - 1, yr: nextMonth.yr });
-            setNextNextMonth({ mon: 12, yr: nextNextMonth.yr - 1 });
-
-            //revert styles
-            setLmS(lmStyles);
-            setSmS(smStyles);
-            setNmS(nmStyles);
-          }, 300);
-
-          break;
-        } else {
-          //move calendars
-          setLmS(lmTransitionR);
-          setSmS(smTransitionR);
-          setNmS(nmTransitionR);
-          setTimeout(() => {
-            //set new data
-            setLastMonth({ mon: lastMonth.mon - 1, yr: selectedMonth.yr });
-            setNextMonth({ mon: selectedMonth.mon, yr: selectedMonth.yr });
-            setNextNextMonth({
-              mon: nextNextMonth.mon - 1,
-              yr: nextNextMonth.yr,
-            });
-            setSelectedMonth({
-              mon: selectedMonth.mon - 1,
-              yr: selectedMonth.yr,
-            });
-            //revert styles
-            setLmS(lmStyles);
-            setSmS(smStyles);
-            setNmS(nmStyles);
-          }, 300);
-
-          break;
-        }
+        break;
       default:
         console.log("please enter + or - to change the month");
         break;
@@ -517,34 +227,20 @@ export const Calendar = ({
         <Month
           month={lastMonth}
           className={lmStyleState}
-          selectedDayStart={selectedDayStart}
-          selectedDayEnd={selectedDayEnd}
+          selectedDay={selectedDay}
           selectDay={selectDay}
-          reserved={reserved}
         />
         <Month
           month={selectedMonth}
           className={smStyleState}
-          selectedDayStart={selectedDayStart}
-          selectedDayEnd={selectedDayEnd}
+          selectedDay={selectedDay}
           selectDay={selectDay}
-          reserved={reserved}
         />
         <Month
           month={nextMonth}
           className={nmStyleState}
-          selectedDayStart={selectedDayStart}
-          selectedDayEnd={selectedDayEnd}
+          selectedDay={selectedDay}
           selectDay={selectDay}
-          reserved={reserved}
-        />
-        <Month
-          month={nextNextMonth}
-          className={nnmStyleState}
-          selectedDayStart={selectedDayStart}
-          selectedDayEnd={selectedDayEnd}
-          selectDay={selectDay}
-          reserved={reserved}
         />
       </div>
     </div>
--- front-end/src/components/Trips.js
+++ front-end/src/components/Trips.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import { Calendar } from "./Calendar";
 export const Trips = () => {
+  const [selectedDay, setSelectedDay] = useState(new Date(2021, 4, 1));
+  console.log(selectedDay);
   return (
     <div className="flex flex-col mt-[96px]">
       <div>
         <h2>Trips</h2>
       </div>
-      <Calendar />
+      <Calendar selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
     </div>
   );
 };
